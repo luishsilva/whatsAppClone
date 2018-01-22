a19ad41a2a50cf75d31390a46d9d95a8e2b97092
luishsilva <luizhrick@gmail.com>
2018-01-22 09:55:14 -0200

    Refatorando o cadastro do usuario no authentication do firebase e no firebase database

--- app/src/main/java/com/example/luissilva/whatsappclone/activity/UserRegisterActivity.java
+++ app/src/main/java/com/example/luissilva/whatsappclone/activity/UserRegisterActivity.java
@@ -1,6 +1,7 @@
 package com.example.luissilva.whatsappclone.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,11 @@ import com.example.luissilva.whatsappclone.R;
 import com.example.luissilva.whatsappclone.model.User;
 import com.example.luissilva.whatsappclone.presenter.PresenterUser;
 import com.example.luissilva.whatsappclone.utils.MessagesUtils;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class UserRegisterActivity extends AppCompatActivity {
 
@@ -18,9 +24,10 @@ public class UserRegisterActivity extends AppCompatActivity {
     private EditText pass;
     private Button btnRegister;
 
-    private User user;
+    private User mUser;
     private PresenterUser presenterUser;
     private MessagesUtils messagesUtils;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,17 +42,31 @@ public class UserRegisterActivity extends AppCompatActivity {
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                user = new User();
-                user.setName(name.getText().toString());
-                user.setEmail(email.getText().toString());
-                user.setPass(pass.getText().toString());
+                mUser = new User();
+                mUser.setName(name.getText().toString());
+                mUser.setEmail(email.getText().toString());
+                mUser.setPass(pass.getText().toString());
 
-                presenterUser = new PresenterUser(UserRegisterActivity.this);
-                if(presenterUser.registerUserAuth(user)){
-                    messagesUtils = new MessagesUtils(UserRegisterActivity.this);
-                    messagesUtils.toastMsg(getString(R.string.registration_success));
-                }
+                presenterUser = new PresenterUser();
+                // register the user on Authetication
+                presenterUser.registerUserAuth(mUser).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            // get user ID at Firebase
+                            FirebaseUser firebaseUser = task.getResult().getUser();
+                            mUser.setId(firebaseUser.getUid());
 
+                            // register user on database
+                            presenterUser.registerUser(mUser).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    finish();
+                                }
+                            });
+                        }
+                    }
+                });
             }
         });
     }
