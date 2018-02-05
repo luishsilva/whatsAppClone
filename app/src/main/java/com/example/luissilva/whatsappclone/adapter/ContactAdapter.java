b436ba0d178d3b7c0367c894189a8bd47a5175ab
Luis Silva <luissilva@MacBook-Pro-de-Luis.local>
2018-02-05 06:49:00 -0200

    finalizando a listagem de contatos

--- app/src/main/java/com/example/luissilva/whatsappclone/adapter/ContactAdapter.java
+++ app/src/main/java/com/example/luissilva/whatsappclone/adapter/ContactAdapter.java
@@ -7,7 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import com.example.luissilva.whatsappclone.R;
 import com.example.luissilva.whatsappclone.model.Contact;
 
 import java.lang.reflect.Array;
@@ -21,6 +23,8 @@ public class ContactAdapter extends ArrayAdapter<Contact>{
 
     private Context mContext;
     private ArrayList<Contact> mContactArrayList;
+    private TextView nome;
+    private TextView email;
 
     public ContactAdapter(Context  pContext, ArrayList<Contact> pContactArrayList) {
         super(pContext,0,pContactArrayList);
@@ -35,10 +39,18 @@ public class ContactAdapter extends ArrayAdapter<Contact>{
         View view = null;
 
         if (mContactArrayList != null){
-            //LayoutInflater inflater = mContext.getSystemService();
-        }
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(mContext.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.cell_item_contacts, parent, false);
 
-        return super.getView(position, convertView, parent);
+            nome  = view.findViewById(R.id.tvNome);
+            email = view.findViewById(R.id.tvEmail);
+
+            Contact contact = mContactArrayList.get(position);
+
+            nome.setText(contact.getNome());
+            email.setText(contact.getEmail());
+        }
+        return view;
     }
 
 }
